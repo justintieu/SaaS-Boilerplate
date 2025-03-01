50eab4f904619574a9c73343fe77b48028537f00
Justin Tieu <justin@provusinc.com>
2025-03-01 00:07:16 -0500

    fix: reorganized schema into separate files per table

---
+++ src/models/ToDo.ts
@@ -0,0 +1,18 @@
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+} from 'drizzle-orm/pg-core';
+
+export const todoSchema = pgTable('todo', {
+  id: serial('id').primaryKey(),
+  ownerId: text('owner_id').notNull(),
+  title: text('title').notNull(),
+  message: text('message').notNull(),
+  updatedAt: timestamp('updated_at', { mode: 'date' })
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
+});
